9302e743aa054d011bf645691caa834e472ae949
itsmeDandoy <153791453+dandoysawi@users.noreply.github.com>
2026-02-05 10:14:34 +0800

    Add user input for name and age with future age calculation

---
+++ Activity 1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: ")) # Convert string input to integer
+
+# Calculate future age
+future_age = age + 1
+
+print(f"Hello, {name}. You will be {future_age} in one year.")
